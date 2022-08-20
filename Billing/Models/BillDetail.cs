d5295a153ca2667661e765460229ea370e203a7d
vikbar11 <vialbaor10@gmail.com>
2022-08-20 00:56:03 -0500

    First commit

---
+++ Billing/Models/BillDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace Billing.Models
+{
+    public partial class BillDetail
+    {
+        public int BillId { get; set; }
+        public int IdOrderStatus { get; set; }
+        public int IdProduct { get; set; }
+        public string Description { get; set; } = null!;
+        public float Quantity { get; set; }
+    }
+}
