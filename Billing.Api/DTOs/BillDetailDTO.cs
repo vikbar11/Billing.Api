f2a727fca6542bf29789123742ad81dbf781670a
vikbar11 <vialbaor10@gmail.com>
2022-08-21 20:30:40 -0500

    Billing.Api (OK)

---
+++ Billing.Api/DTOs/BillDetailDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Billing.Api.DTOs
+{
+    public class BillDetailDTO
+    {
+        public int IdBillDetailDTO { get; set; }
+        public string? DescriptionDTO { get; set; }
+        public int IdBillDTO { get; set; }
+        public int IdProductDTO { get; set; }
+        public int QuantityDTO { get; set; }
+    }
+}
---
+++ Billing.Api/DTOs/BillDetailInvDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Billing.Api.DTOs
+{
+    public class BillDetailInvDTO
+    {
+        public decimal PriceDTO { get; set; }
+        public int QuantityDTO { get; set; }
+        public string ProductNameDTO { get; set; }
+    }
+}
