12523fc0cfd8f7ac509a0f2b444733996e8d9b15
vikbar11 <vialbaor10@gmail.com>
2022-08-21 20:27:29 -0500

    Directorio limpio

--- Billing.Api/DTOs/BillDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Billing.Api.DTOs
-{
-    public class BillDTO
-    {
-        public int IdBillDTO { get; set; }
-        public int IdCustomerDTO { get; set; }
-        public DateTime BillDateDTO { get; set; }
-    }
-}
--- Billing.Api/DTOs/BillDetailInvDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Billing.Api.DTOs
-{
-    public class BillDetailInvDTO
-    {
-        public decimal PriceDTO { get; set; }
-        public int QuantityDTO { get; set; }
-        public string ProductNameDTO { get; set; }
-    }
-}
